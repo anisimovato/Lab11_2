c45ea01799b3667479da52ed21e51abcc91a5f4f
Татьяна Анисимова <kulmina@mail.ru>
2021-11-27 18:00:54 +0300

    Добавьте файлы проекта.

---
+++ Lab11HomeW/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab11uravn;
+
+namespace Lab11HomeW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uravnenie uravnenie = new Uravnenie(60, 20);
+            uravnenie.X();
+            Console.ReadKey();
+        }
+        
+    }
+}
